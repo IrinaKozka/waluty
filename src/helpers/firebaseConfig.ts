ccc3a7d1407cdbecb025966c6de2b2e98eef4080
IrinaKozka <109234531+IrinaKozka@users.noreply.github.com>
2022-11-21 19:22:58 +0100

    21.11.22

---
+++ src/helpers/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMAQpNS__5U-KTbYhumhFqjGGGEug8XzU",
+  authDomain: "przelicznik-57660.firebaseapp.com",
+  projectId: "przelicznik-57660",
+  storageBucket: "przelicznik-57660.appspot.com",
+  messagingSenderId: "22857711878",
+  appId: "1:22857711878:web:3ffa63e74ffe6c4df55e87",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
