4986f4b0fa6621445aa563e6e9bf4a47978267b7
IrinaKozka <109234531+IrinaKozka@users.noreply.github.com>
2022-11-21 20:21:30 +0100

    21.11.2022 20.21

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from "react";
+// import Navbar from "./components/Navbar/Navbar";
+// import HomePage from "./components/HomePage/HomePage";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import RegisterPage from "./components/RegisterPage/RegisterPage";
+// import LoginPage from "./components/LoginPage/LoginPage";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { auth } from "./firebaseConfig";
+// import UserPage from "./components/UserPage/UserPage";
+// import SearchPage from "./components/SearchPage/SearchPage";
+// import { authContext } from "./helpers/authContext";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <authContext.Provider value={loggedIn}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+          <HomePage />
+        </authContext.Provider>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/LoginPage/LoginPage.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Typography, Button } from "@mui/material";
+// import { Link } from "react-router-dom";
+// import LoginForm from "../LoginForm/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <>
+      <LoginForm />
+      <Typography
+        variant="h6"
+        sx={{
+          fontWeight: "100",
+          my: ".8rem",
+          color: "red",
+          mx: "auto",
+          textAlign: "center",
+        }}
+      >
+        Don't have an account yet? Register now!
+      </Typography>
+      <Link to="/register" style={{ textDecoration: "none" }}>
+        <Button sx={{ display: "block", mx: "auto" }}>Register</Button>
+      </Link>
+    </>
+  );
+}
+
+export default LoginPage
