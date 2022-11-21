a7eb02ce3bd11d5e3486ff2eb64fd0d57d67118e
monikahan <109234400+monikahan@users.noreply.github.com>
2022-11-21 21:25:38 +0100

    21:25 MH

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-// import { useState } from "react";
+import { useState } from "react";
 // import Navbar from "./components/Navbar/Navbar";
 // import HomePage from "./components/HomePage/HomePage";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import RegisterPage from "./components/RegisterPage/RegisterPage";
 // import LoginPage from "./components/LoginPage/LoginPage";
 // import { onAuthStateChanged } from "firebase/auth";
-// import { auth } from "./firebaseConfig";
+import { auth } from "../src/helpers/firebaseConfig";
 // import UserPage from "./components/UserPage/UserPage";
 // import SearchPage from "./components/SearchPage/SearchPage";
-// import { authContext } from "./helpers/authContext";
+import { authContext } from "../src/helpers/authContext";
 
 
 function App() {
