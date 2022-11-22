455059b09728e5a79eaca2c6e46c0cb29bb6ff83
IrinaKozka <109234531+IrinaKozka@users.noreply.github.com>
2022-11-22 19:09:37 +0100

    1

--- src/helpers/firebaseConfig.ts
+++ src/helpers/firebaseConfig.ts
@@ -1,13 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-import {getStorage} from 'firebase/storage'
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDMAQpNS__5U-KTbYhumhFqjGGGEug8XzU",
@@ -19,5 +15,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-<<<<<<< HEAD
+
 const app = initializeApp(firebaseConfig);
